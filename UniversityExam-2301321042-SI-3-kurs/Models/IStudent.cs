ebf6192710421f986841ee358e09a935bd925e8e
Sedyankoff <stoil0878@gmail.com>
2026-06-01 18:39:11 +0300

    Added student models

---
+++ UniversityExam-2301321042-SI-3-kurs/Models/IStudent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniversityExam_2301321042_SI_3_kurs.Models;
+
+public interface IStudent
+{
+    string Name { get; }
+
+    void ShowInfo();
+}
---
+++ UniversityExam-2301321042-SI-3-kurs/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UniversityExam_2301321042_SI_3_kurs.Strategy;
+
+namespace UniversityExam_2301321042_SI_3_kurs.Models;
+
+public class Student : IStudent
+{
+    private readonly IArrivalStrategy _arrivalStrategy;
+
+    public string Name { get; }
+
+    public Student(
+        string name,
+        IArrivalStrategy arrivalStrategy)
+    {
+        Name = name;
+        _arrivalStrategy = arrivalStrategy;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Студент: {Name}");
+        _arrivalStrategy.Arrive();
+    }
+}
